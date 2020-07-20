117d3040d0af2bff6445029519f5a014b9fd2952
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-07-19 21:14:53 -0700

    Implemented client

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {
+    Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container
+} from 'reactstrap';
+
+class AppNavBar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">TodoList</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/kaavyalakshmanan">
+                                    Github
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        );  
+    }
+}
+
+export default AppNavBar;
