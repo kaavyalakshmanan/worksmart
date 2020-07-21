9dbb9ea9f0e5f9221a1dbccb0480a38ca7ca3d82
kaavyalakshmanan <kaavya.laks@gmail.com>
2020-07-20 20:36:46 -0700

    Close modal after registering successful user

--- client/src/components/auth/RegisterModal.js
+++ client/src/components/auth/RegisterModal.js
@@ -24,7 +24,7 @@ class RegisterModal extends Component {
     };
 
     componentDidUpdate(prevProps) {
-        const {error} = this.props;
+        const {error, isAuthenticated} = this.props;
         if (error !== prevProps.error) {
             // Check for register error
             if (error.id === 'REGISTER_FAIL') {
@@ -33,7 +33,15 @@ class RegisterModal extends Component {
                 this.setState({msg: null});
             }
         }
+
+        // If authenticated, close modal
+        if (this.state.modal) {
+            if (isAuthenticated) {
+                this.toggle();
+            }
+        }
     }
+
 
     toggle = () => {
         // Clear errors
